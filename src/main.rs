d9de15a9846695d51d1e37711765cc3a340c5e82
oddgrd <oddgrd@gmail.com>
2022-03-24 19:02:14 +0100

    App and test setup, first route and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000.");
+
+    zero2prod::run(listener)?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,31 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{}/health_check", address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
