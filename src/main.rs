281f5ddeeefaf4688b726cff3c6b8c4711320ae2
oddgrd <oddgrd@gmail.com>
2022-04-02 22:25:22 +0200

    Switch to tokio::main

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
+
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
