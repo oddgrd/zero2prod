4892dc9b3de25b7fefb5c9a7e164d2c6b190dcc4
oddgrd <oddgrd@gmail.com>
2022-04-03 20:51:59 +0200

    Set log level in env

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "trace".into(), std::io::stdout);
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
